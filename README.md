98c324c3e1324ddcf9fe209911976ff5c94be947
leongjanine <63428072+lumpycheese@users.noreply.github.com>
2026-04-22 22:22:26 -0500

    more progress (in_zone, send_email)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# programming-challenge
-pip install -r requirements.txt
-python service.py
+## programming-challenge
+ pip install -r requirements.txt
+
+ python service.py
--- service.py
+++ service.py
@@ -2,11 +2,13 @@ import requests
 import time
 import json
 from shapely.geometry import shape, Point
-
-
+import smtplib, ssl
+from email.message import EmailMessage
 
 api_url = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test"
-email_address = "sprinter-eng-test@guerrillamail.info"
+recipient_email = "sprinter-eng-test@guerrillamail.info"
+sender_email = "testemailforjl@gmail.com"
+password = "nclpoyahimpfabsd"
 
 # GET from api (api_url) to get clinician status data for clinician #clinicianID
 def get_clinician_status(clinicianID):
@@ -17,25 +19,55 @@ def get_clinician_status(clinicianID):
     return data
   else:
     print("Alert! Endpoint did not return: {response.status_code}")
-  print(response)
 
 # check if clinician is in safe zone
 def in_zone(clinician_status):
+  x,y = clinician_status['features'][0]['geometry']['coordinates']
+  point = Point(x,y)
+  # print(f"point {point}")
+
+  for feature in clinician_status['features']:
+    if feature["geometry"]["type"]=="Polygon": 
+      polygon = shape(feature['geometry'])
+      # print(polygon)
+      if polygon.intersects(point):
+        return True
+  return False
   
 
 # send email to email_address with missing phlebotomist’s ID
-def send_email(email_address):
+def send_email(sender_email, recipient_email, body):
+  print(f"sender address: {sender_email}\nreceiving address: {recipient_email}\nbody: {body}")
+  textfile = "email.txt"
+    
+  with open(textfile, "w") as f:
+    f.write(body)
+  with open(textfile) as fp:
+    msg = EmailMessage()
+    msg.set_content(fp.read())
+
+  msg['Subject'] = f'alert'
+  msg['From'] = sender_email
+  msg['To'] = recipient_email
+
+  with smtplib.SMTP('smtp.gmail.com', 587) as s:
+    s.starttls()
+    s.login(sender_email, password)
+    s.send_message(msg)
 
 
 def main():
-  testIDs = [1,2,3,4,5,6,7]
-
+  clinicianIDs = [1,2,3,4,5,6]
   while True:
-    for i in range(len(testIDs)):
-      print(f"clinician {testIDs[i]} status:")
-      clinician_status = get_clinician_status(testIDs[i])
-      print(json.dumps(clinician_status, indent=4))
+    for i in range(len(clinicianIDs)):
+      print(f"clinician {clinicianIDs[i]} status:")
+      clinician_status = get_clinician_status(clinicianIDs[i])
+      print(f"in zone? {in_zone(clinician_status)}")
+      if not in_zone(clinician_status):
+        send_email(sender_email, recipient_email, f"missing clinician: {clinicianIDs[i]}")
+      print(json.dumps(clinician_status, indent=2))
     time.sleep(10)
+  # send_email(sender_email, recipient_email, "test email")
 
 if __name__ == "__main__":
-    main()
+  main()
