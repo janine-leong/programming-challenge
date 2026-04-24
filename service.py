783832736c11b66939b12c2db7ab612b337ea749
leongjanine <63428072+lumpycheese@users.noreply.github.com>
2026-04-24 11:06:15 -0500

    updates

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ import requests
 import time
 import json
 from shapely.geometry import shape, Point
-import smtplib, ssl
+import smtplib
 from email.message import EmailMessage
 
 api_url = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test"
@@ -12,13 +12,15 @@ password = "nclpoyahimpfabsd"
 
 # GET from api (api_url) to get clinician status data for clinician #clinicianID
 def get_clinician_status(clinicianID):
-  url = f"{api_url}/clinicianstatus/{clinicianID}"
-  response = requests.get(url)
-  if response.status_code == 200:
+  try:
+    url = f"{api_url}/clinicianstatus/{clinicianID}"
+    response = requests.get(url)
+    response.raise_for_status() 
     data = response.json()
     return data
-  else:
-    print("Alert! Endpoint did not return: {response.status_code}")
+  except Exception as e:
+    print(f"Alert! Endpoint did not return: {e}")
+    return None
 
 # check if clinician is in safe zone
 def in_zone(clinician_status):
@@ -37,19 +39,22 @@ def in_zone(clinician_status):
 # send email to email_address with missing phlebotomist’s ID
 def send_email(sender_email, recipient_email, body):
   print(f"sender address: {sender_email}\nreceiving address: {recipient_email}\nbody: {body}")
-  textfile = "email.txt"
-  with open(textfile, "w") as f:
-    f.write(body)
-  with open(textfile) as fp:
-    msg = EmailMessage()
-    msg.set_content(fp.read())
-  msg['Subject'] = f'alert'
-  msg['From'] = sender_email
-  msg['To'] = recipient_email
-  with smtplib.SMTP('smtp.gmail.com', 587) as s:
-    s.starttls()
-    s.login(sender_email, password)
-    s.send_message(msg)
+  try:
+    textfile = "email.txt"
+    with open(textfile, "w") as f:
+      f.write(body)
+    with open(textfile) as fp:
+      msg = EmailMessage()
+      msg.set_content(fp.read())
+    msg['Subject'] = f'alert'
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    with smtplib.SMTP('smtp.gmail.com', 587) as s:
+      s.starttls()
+      s.login(sender_email, password)
+      s.send_message(msg)
+  except Exception as e:
+    print(f"send_email exception: {e}")
 
 
 def main():
@@ -58,10 +63,11 @@ def main():
     for i in range(len(clinicianIDs)):
       print(f"clinician {clinicianIDs[i]} status:")
       clinician_status = get_clinician_status(clinicianIDs[i])
-      print(f"in zone? {in_zone(clinician_status)}")
-      if not in_zone(clinician_status):
-        send_email(sender_email, recipient_email, f"missing clinician: {clinicianIDs[i]}")
-      print(json.dumps(clinician_status, indent=2))
+      if clinician_status:
+        print(f"in zone? {in_zone(clinician_status)}")
+        if not in_zone(clinician_status):
+          send_email(sender_email, recipient_email, f"missing clinician: {clinicianIDs[i]}")
+        print(json.dumps(clinician_status, indent=2))
     time.sleep(10)
   # send_email(sender_email, recipient_email, "test email")
 
